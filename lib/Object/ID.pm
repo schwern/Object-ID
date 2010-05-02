997ae5e4d9833f47f80beb8ef33ea1283033453e
Michael G. Schwern <schwern@pobox.com>
2010-05-02 10:28:22 -0700

    Version 0.0.5

--- lib/Object/ID.pm
+++ lib/Object/ID.pm
@@ -5,7 +5,7 @@ use 5.10.0;
 use strict;
 use warnings;
 
-use version; our $VERSION = qv("v0.0.4");
+use version; our $VERSION = qv("v0.0.5");
 
 use Hash::Util::FieldHash;
 use Sub::Name qw(subname);
